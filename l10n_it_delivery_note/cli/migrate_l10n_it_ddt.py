db40ce5b11d5f43512a134c1eb9f16e662107a5f
tafaRU <alex.comba@agilebg.com>
2023-09-21 12:22:05 +0200

    [FIX] Do not migrate fields related to weight
    Otherwise we get the following error:
    
    Traceback (most recent call last):
      File "/srv/odoo/.pyenv/versions/3.10.8/lib/python3.10/site-packages/odoo/addons/l10n_it_delivery_note/cli/core.py", line 85, in run
        self.execute()
      File "/srv/odoo/.pyenv/versions/3.10.8/lib/python3.10/site-packages/odoo/addons/l10n_it_delivery_note/cli/migrate_l10n_it_ddt.py", line 349, in execute
        self.migrate_documents()
      File "/srv/odoo/.pyenv/versions/3.10.8/lib/python3.10/site-packages/odoo/addons/l10n_it_delivery_note/cli/migrate_l10n_it_ddt.py", line 317, in migrate_documents
        delivery_note = DeliveryNote.create(vals_getter(document))
      File "/srv/odoo/.pyenv/versions/3.10.8/lib/python3.10/site-packages/odoo/addons/l10n_it_delivery_note/cli/migrate_l10n_it_ddt.py", line 287, in vals_getter
        "gross_weight": record.gross_weight or record.weight,
      File "/srv/odoo/.pyenv/versions/3.10.8/lib/python3.10/site-packages/odoo/fields.py", line 1209, in __get__
        self.compute_value(recs)
      File "/srv/odoo/.pyenv/versions/3.10.8/lib/python3.10/site-packages/odoo/fields.py", line 1387, in compute_value
        records._compute_field_value(self)
      File "/srv/odoo/.pyenv/versions/3.10.8/lib/python3.10/site-packages/odoo/models.py", line 4215, in _compute_field_value
        fields.determine(field.compute, self)
      File "/srv/odoo/.pyenv/versions/3.10.8/lib/python3.10/site-packages/odoo/fields.py", line 97, in determine
        return needle(*args)
      File "/srv/odoo/migrate_l10nitddt/l10n_it_ddt/models/stock_picking_package_preparation.py", line 238, in _compute_weight
        super(StockPickingPackagePreparation, self)._compute_weight()
      File "/srv/odoo/migrate_l10nitddt/stock_picking_package_preparation/models/stock_picking_package_preparation.py", line 116, in _compute_weight
        quants = package._get_contained_quants()
    AttributeError: 'stock.quant.package' object has no attribute '_get_contained_quants'

--- l10n_it_delivery_note/cli/migrate_l10n_it_ddt.py
+++ l10n_it_delivery_note/cli/migrate_l10n_it_ddt.py
@@ -308,12 +308,12 @@ class MigrateL10nItDdt(EasyCommand):
                 "packages": record.parcels,
                 "volume": record.volume,
                 # "volume_uom_id": record.volume_uom_id.id or self._default_volume_uom.id,
-                "gross_weight": record.gross_weight or record.weight,
-                "gross_weight_uom_id": record.gross_weight_uom_id.id
-                or self._default_weight_uom.id,
-                "net_weight": record.weight_manual or record.weight,
-                "net_weight_uom_id": record.weight_manual_uom_id.id
-                or self._default_weight_uom.id,
+                # "gross_weight": record.gross_weight or record.weight,
+                # "gross_weight_uom_id": record.gross_weight_uom_id.id
+                # or self._default_weight_uom.id,
+                # "net_weight": record.weight_manual or record.weight,
+                # "net_weight_uom_id": record.weight_manual_uom_id.id
+                # or self._default_weight_uom.id,
                 "goods_appearance_id": self._goods_descriptions[
                     record.goods_description_id
                 ].id,
