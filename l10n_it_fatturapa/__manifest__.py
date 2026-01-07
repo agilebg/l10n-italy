57bf14b9178ce80c5eecede3fb09eafa5d5f926b
Alberto Re <alberto.re@agilebg.com>
2026-01-07 17:31:07 +0100

    [MIG] l10n_it_fatturapa: Migration to 18.0

--- l10n_it_fatturapa/__manifest__.py
+++ l10n_it_fatturapa/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     "name": "ITA - Fattura elettronica - Base",
-    "version": "16.0.1.4.1",
+    "version": "18.0.1.0.0",
     "category": "Localization/Italy",
     "summary": "Fatture elettroniche",
     "author": "Davide Corio, Agile Business Group, Innoviu, "
