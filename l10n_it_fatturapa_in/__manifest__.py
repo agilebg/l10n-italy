eb50b958a1c5cc1a459df4b063caaf77077a2074
Alberto Re <alberto.re@agilebg.com>
2026-01-09 16:19:40 +0100

    Barebone module

---
+++ l10n_it_fatturapa_in/__manifest__.py
@@ -0,0 +1,26 @@
+# Copyright 2015 AgileBG SAGL <http://www.agilebg.com>
+# Copyright 2015 innoviu Srl <http://www.innoviu.com>
+# Copyright 2018 Lorenzo Battistini
+# Copyright 2018 Sergio Zanchetta (Associazione PNLUG - Gruppo Odoo)
+# Copyright 2022 Marco Colombo (<marco.colombo@phi.technology>)
+# Copyright 2024 Simone Rubino - Aion Tech
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "ITA - Fattura elettronica - Ricezione",
+    "version": "18.0.1.0.0",
+    "development_status": "Beta",
+    "category": "Localization/Italy",
+    "summary": "Ricezione fatture elettroniche",
+    "author": "Agile Business Group, Innoviu, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-italy" "l10n_it_fatturapa_in",
+    "license": "AGPL-3",
+    "maintainers": ["MarcoCalcagni", "Borruso"],
+    "depends": [
+    ],
+    "data": [
+    ],
+    "installable": True,
+    "external_dependencies": {
+    },
+}
