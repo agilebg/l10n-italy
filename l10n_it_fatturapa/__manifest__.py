abfe3a3ae28cb543ec15e7d56e9b2a1f4e032bd7
Alberto Re <alberto.re@agilebg.com>
2026-01-09 11:34:05 +0100

    Barebone module

---
+++ l10n_it_fatturapa/__manifest__.py
@@ -0,0 +1,26 @@
+# Copyright 2014 Davide Corio <davide.corio@abstract.it>
+# Copyright 2015-2016 Lorenzo Battistini - Agile Business Group
+# Copyright 2018-2019 Sergio Zanchetta (Associazione PNLUG - Gruppo Odoo)
+# Copyright 2019 Gianluigi Tiesi - Netfarm S.r.l.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "ITA - Fattura elettronica - Base",
+    "version": "18.0.1.0.0",
+    "category": "Localization/Italy",
+    "summary": "Fatture elettroniche",
+    "author": "Davide Corio, Agile Business Group, Innoviu, "
+    "Odoo Italia Network, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-italy",
+    "license": "AGPL-3",
+    "excludes": [
+    ],
+    "depends": [
+    ],
+    "data": [
+    ],
+    "installable": True,
+    "external_dependencies": {
+    },
+    "development_status": "Beta",
+}
