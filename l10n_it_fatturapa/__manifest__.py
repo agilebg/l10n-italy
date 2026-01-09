0da14e935576921cacd12a0ec17d62e921c1b0ef
Alberto Re <alberto.re@agilebg.com>
2026-01-09 15:53:44 +0100

    Bump version according to our guidelines
    See https://www.notion.so/Come-versionare-le-PR-aperte-su-OCA-18fad3b83b8f80af98fbe4d119ea2ffc.

--- l10n_it_fatturapa/__manifest__.py
+++ l10n_it_fatturapa/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     "name": "ITA - Fattura elettronica - Base",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.3.20260109",
     "category": "Localization/Italy",
     "summary": "Fatture elettroniche",
     "author": "Davide Corio, Agile Business Group, Innoviu, "
