768fdf777c730da83c259f36b476feb71c8866bd
Alberto Re <alberto.re@agilebg.com>
2026-01-12 17:08:09 +0100

    Bump version according to our guidelines
    See https://www.notion.so/Come-versionare-le-PR-aperte-su-OCA-18fad3b83b8f80af98fbe4d119ea2ffc.

--- l10n_it_fatturapa_in/__manifest__.py
+++ l10n_it_fatturapa_in/__manifest__.py
@@ -8,7 +8,7 @@
 
 {
     "name": "ITA - Fattura elettronica - Ricezione",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.4.20260112",
     "development_status": "Beta",
     "category": "Localization/Italy",
     "summary": "Ricezione fatture elettroniche",
