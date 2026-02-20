078006570fb5e15c05ddcddd6dadb440f011326d
Alberto Re <alberto.re@agilebg.com>
2026-02-20 15:23:46 +0100

    Reintroduce field needed by l10n_it_edi_extension init hooks
    The field is required here:
    https://github.com/OCA/l10n-italy/blob/cfe9d356ad5f33d4f7435a5889a43f0b52afa835/l10n_it_edi_extension/__init__.py#L811C19-L811C34

--- l10n_it_fatturapa_out/models/account.py
+++ l10n_it_fatturapa_out/models/account.py
@@ -9,6 +9,17 @@ from odoo import api, fields, models
 class AccountInvoice(models.Model):
     _inherit = "account.move"
 
+    fatturapa_state = fields.Selection(
+        [
+            ("ready", "Ready to Send"),
+            ("sent", "Sent"),
+            ("delivered", "Delivered"),
+            ("accepted", "Accepted"),
+            ("error", "Error"),
+        ],
+        string="E-invoice State",
+        store="true",
+    )
     fatturapa_payment_method_id = fields.Many2one(
         comodel_name="fatturapa.payment_method",
         string="Fiscal Payment Method",
