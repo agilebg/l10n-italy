a05721e829667e68d7a7c17553921a3dd8782092
Alberto Re <alberto.re@agilebg.com>
2026-02-20 15:24:29 +0100

    Bump version number

--- l10n_it_fatturapa_out/__manifest__.py
+++ l10n_it_fatturapa_out/__manifest__.py
@@ -8,7 +8,7 @@
 
 {
     "name": "ITA - Fattura elettronica - Emissione",
-    "version": "18.0.1.6.20260214",
+    "version": "18.0.1.6.202602201531",
     "development_status": "Beta",
     "category": "Localization/Italy",
     "summary": "Emissione fatture elettroniche",
