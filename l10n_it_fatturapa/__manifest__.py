6c692b15a57f206c210226444942ff06e84ed71a
Alberto Re <alberto.re@agilebg.com>
2026-03-05 14:41:41 +0100

    Bump version according to our guidelines

--- l10n_it_fatturapa/__manifest__.py
+++ l10n_it_fatturapa/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     "name": "ITA - Fattura elettronica - Base",
-    "version": "18.0.1.3.20260305",
+    "version": "18.0.1.3.202603051632",
     "category": "Localization/Italy",
     "summary": "Fatture elettroniche",
     "author": "Davide Corio, Agile Business Group, Innoviu, "
